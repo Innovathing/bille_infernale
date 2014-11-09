d3b568a28d065e282a10d378ababe9f6745a7007
David Livet <david.livet@orange.fr>
2014-11-09 12:50:46 +0100

    Configuration des horloges (ajouts des constantes)

--- src/services_applicatives/arbre_horloge.c
+++ src/services_applicatives/arbre_horloge.c
@@ -4,6 +4,80 @@
 
 void arbre_horloge_init() {
 	CLOCK_Configure();
+
+	#ifdef AH_TIM2
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+	#endif
+
+	#ifdef AH_TIM3
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+	#endif
+
+	#ifdef AH_TIM4
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+	#endif
+
+	#ifdef AH_TIM5
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM5, ENABLE);
+	#endif
+
+	#ifdef AH_TIM6
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, ENABLE);
+	#endif
+
+	#ifdef AH_TIM7
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM7, ENABLE);
+	#endif
+
+	#ifdef AH_TIM1
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
+	#endif
+
+	#ifdef AH_TIM8
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM8, ENABLE);
+	#endif
+
+	#ifdef AH_ADC1
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+	#endif
+
+	#ifdef AH_ADC2
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC2, ENABLE);
+	#endif
+
+	#ifdef AH_ADC3
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC3, ENABLE);
+	#endif
+
+	#ifdef AH_GPIOA
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+	#endif
+
+	#ifdef AH_GPIOB
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+	#endif
+
+	#ifdef AH_GPIOC
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	#endif
+
+	#ifdef AH_GPIOD
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+	#endif
+
+	#ifdef AH_GPIOE
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE, ENABLE);
+	#endif
+
+	#ifdef AH_GPIOF
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOF, ENABLE);
+	#endif
+
+	#ifdef AH_GPIOG
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOG, ENABLE);
+	#endif
+
+	#ifdef AH_AFIO
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+	#endif
 }
--- src/services_applicatives/arbre_horloge.h
+++ src/services_applicatives/arbre_horloge.h
@@ -1,6 +1,29 @@
-#ifndef ARBRE_HORLOGE_H
-#define ARBRE_HORLOGE_H
+/**
+ * \file
+ * \brief Gestion de l'arbre d'horloge pour le projet
+ * \author Hugo DJEMAA (INSA - Toulouse, France)
+ * \author Jérémie BOUTOILLE (INSA - Toulouse, France)
+ * \author Mickael GOUBIN (INSA - Toulouse, France)
+ * \author David LIVET (INSA - Toulouse, France)
+ * \version 1.0
+ * \date 09/10/2014
+ *
+ * Ce service applicatif active les arbres d'horloges dont le projet à besoin
+ *
+ */
 
+#ifndef __ARBRE_HORLOGE_H
+#define __ARBRE_HORLOGE_H
 
+/**
+* \brief Active les horloges
+*
+* Active les horloges à l'aide des constantes suivantes :
+* - Timers : AH_TIM1, AH_TIM2, AH_TIM3, AH_TIM4, AH_TIM5, AH_TIM6, AH_TIM7, AH_TIM8
+* - Adc : AH_ADC1, AH_ADC2, AH_ADC3
+* - GPIO : AH_GPIOA, AH_GPIOB, AH_GPIOC, AH_GPIOD, AH_GPIOE, AH_GPIOF, AH_GPIOG
+* - AH_AFIO
+*/
+void arbre_horloge_init();
 
 #endif
